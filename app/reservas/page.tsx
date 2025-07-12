725d91355a5ca25cfa2d7035eb6747c6db2bd00e
Luis J. Centeno <luisjcenteno@live.com>
2025-07-11 20:03:43 -0600

    fix: update revalidation period to 1 hour for ReservasPage

--- app/reservas/page.tsx
+++ app/reservas/page.tsx
@@ -1,7 +1,7 @@
 import { getPackages } from '@/lib/pocketbase'
 import ReservationForm from '@/components/reservation-form'
 
-export const revalidate = 24 * 60 * 60 // Revalidate every 24 hours
+export const revalidate = 3600 // Revalidate every hour
 
 export default async function ReservasPage() {
     const packages = await getPackages()
