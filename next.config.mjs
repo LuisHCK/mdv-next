61ffd61aa82a1c0da8aadf28ebc233c5de084212
Luis J. Centeno <luisjcenteno@live.com>
2025-07-15 16:30:40 -0600

    refactor: clean up formatting in next.config.mjs for consistency

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-  output: 'standalone',
+    eslint: {
+        ignoreDuringBuilds: true
+    },
+    typescript: {
+        ignoreBuildErrors: true
+    },
+    images: {
+        unoptimized: true
+    },
+    redirects: async () => {
+        return [
+            {
+                source: '/qr',
+                destination: '/',
+                permanent: false
+            }
+        ]
+    },
+    output: 'standalone'
 }
 
 export default nextConfig
